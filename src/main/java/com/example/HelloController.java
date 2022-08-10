ab6180ad978a9671e05745d1d9bab0a6fb838677
Kim <Allen.Kim1@ehealthontario.on.ca>
2022-08-10 11:09:17 -0400

    WIP

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Base64;
 import java.util.HashMap;
@@ -12,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.itextpdf.dito.sdk.core.data.JsonData;
+import com.itextpdf.dito.sdk.license.DitoLicense;
+import com.itextpdf.dito.sdk.output.PdfProducer;
 import com.itextpdf.html2pdf.ConverterProperties;
 import com.itextpdf.html2pdf.HtmlConverter;
 
@@ -31,6 +36,25 @@ public class HelloController {
         HtmlConverter.convertToPdf(html, target, converterProperties);
 
         return target.toByteArray();
+    }
+
+    private void getPdfFromTemplate() throws IOException {
+        // File licenseKeyFile = new File("c:\\Users\\allen.kim1\\da755a010fd6dd733c9d0710390d682e3b6c1c0792a77b67a5e2084afe0449de.json"); 
+        // DitoLicense.loadLicenseFile(licenseKeyFile);
+
+        File templatePackageFile = new File("0265-82.pdf");
+        String templateName = "output";
+
+        FileOutputStream fos = new FileOutputStream(new File("output.pdf"));
+        String json = "{\"first\": \"Allen\"}";
+
+        PdfProducer.convertTemplateFromPackage(templatePackageFile, templateName, fos, new JsonData(json));
+    }
+
+    @RequestMapping( value = "/runTest", method = RequestMethod.GET )
+    public ResponseEntity<Object> runTest() throws Exception {
+        this.getPdfFromTemplate();
+        return ResponseEntity.ok().body("OK");
     }
 
     @RequestMapping( value = "/html2pdf", method = RequestMethod.POST )
